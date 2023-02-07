61bbe2a1a5c625146accacd4a29925bfa954f6d9
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-02-07 10:41:09 -0500

    almost complete

--- src/index.js
+++ src/index.js
@@ -13,41 +13,76 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-const toyCollection = document.querySelector('#toy-collection')
- console.log(toyCollection)
+  const toyCollection = document.querySelector("#toy-collection");
+  const toyForm = document.querySelector(".add-toy-form");
 
-fetch("http://localhost:3000/toys")
-  .then( resp => resp.json())
-  .then( toys => { 
-    toys.forEach(renderToy)
-  })
+  fetch("http://localhost:3000/toys")
+    .then((resp) => resp.json())
+    .then((toys) => {
+      toys.forEach(renderToy);
+    });
 
+  function renderToy(toy) {
+    const card = document.createElement("div");
+    card.className = "card";
 
-  function renderToy (toy) {
-    const card = document.createElement('div')
-    card.className = 'card';
+    const cardH2 = document.createElement("h2");
+    cardH2.textContent = toy.name;
+    card.appendChild(cardH2);
 
-    const cardH2 = document.createElement('h2')
-    cardH2.textContent = toy.name
-    card.appendChild(cardH2)
+    const cardImage = document.createElement("img");
+    cardImage.src = toy.image;
+    cardImage.className = "toy-avatar";
+    card.appendChild(cardImage);
 
-    const cardImage = document.createElement('img')
-    cardImage.src = toy.image
-    cardImage.className = "toy-avatar"
-    card.appendChild(cardImage)
+    const cardP = document.createElement("p");
+    cardP.textContent = `${toy.likes} Likes`;
+    card.appendChild(cardP);
 
-    const cardP = document.createElement('p')
-    cardP.textContent = `${toy.likes} Likes`
-    card.appendChild(cardImage)
+    const cardButton = document.createElement("btn");
+    cardButton.className = "like-btn";
+    cardButton.id = toy.id;
+    cardButton.textContent = "Like ❤️";
 
-    const cardButton = document.createElement('btn')
-    cardButton.className = "like-btn"
-    cardButton.id = toy.id
-    cardButton.textContent = "Like ❤️"
-    card.appendChild(cardButton)
+    cardButton.addEventListener("click", () => {
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          likes: (toy.likes += 1),
+        }),
+      })
+        .then((resp) => resp.json())
+        .then((toy) => {
+          cardP.textContent = `${toy.likes} Likes`;
+        });
+    });
 
-    toyCollection.appendChild(card)
+    card.appendChild(cardButton);
+
+    toyCollection.appendChild(card);
   }
 
-
+  toyForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const toy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0,
+    };
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(toy),
+    })
+      .then((resp) => resp.json())
+      .then((toy) => {
+        renderToy(toy);
+      });
+    toyForm.reset();
+  });
 });
